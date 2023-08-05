e816b345e14cbfb10b89c06470a5d16ce8b32ac1
Teodora Miu <teodora.miu01@gmail.com>
2023-08-05 16:51:55 +0300

    finished file-explorer todos

--- file-explorer/src-tauri/src/main.rs
+++ file-explorer/src-tauri/src/main.rs
@@ -2,54 +2,18 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::env::current_dir;
-use std::fs::{File, write, read_dir};
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn get_current_directory() -> String {
-    match current_dir() {
-        Ok(directory) => return directory.into_os_string().into_string().unwrap(),
-        Err(_error) => panic!("there is no current directory!")
-    }
-}
+use std::fs::{File, write, read_dir, read_to_string};
 
 #[tauri::command]
-fn create_new_file(file_name: String) -> Result<(), String> {
-    let try_file = File::create(file_name);
-    
-    match try_file {
-        Ok(_file) => return Ok(()),
-        Err(_error) => return Err(String::from("The file couldn't be created!"))
-    }
-}
-
-#[tauri::command]
-fn write_to_file(file_name: String, file_contents: String) -> Result<(), String> {
-    let try_open_file = File::open(&file_name);
-
-    match try_open_file {
-        Ok(_file) => {
-            write(&file_name, file_contents).unwrap();
-            Ok(())
-        },
-        Err(_error) => return Err(String::from("The file couldn't be opened!"))
-    }
-}
-
-#[tauri::command]
-fn get_current_files(current_dir: String) -> Result<Vec<String>, String> {
-    let mut files = Vec::new();
-    for file in read_dir(current_dir).unwrap() {
-        if let Ok(file) = file {
-            files.push(file.file_name().into_string().unwrap());
-        }
-    }
-    Ok(files)
+fn example_command() {
+    println!("I like pizza");
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_current_directory, create_new_file, write_to_file, get_current_files])
+        .invoke_handler(tauri::generate_handler![
+            example_command
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- file-explorer/src/App.vue
+++ file-explorer/src/App.vue
@@ -2,29 +2,8 @@
 import {invoke} from "@tauri-apps/api"
 import {ref, Ref} from "vue";
 
-const currentDir: Ref<string> = ref("")
+const filePath: Ref<string> = ref("")
 const files: Ref<string[]> = ref([])
-
-/* TO-DO (3)
-  Show the current directory on the screen.
-*/
-
-/* TO-DO (4)
-  Show the contents of the current directory on the screen.
-  Once a new file is created by the user, it should also appear on the screen.
-*/
-
-/* TO-DO (5)
-  Allow the user to view and edit any text file by clicking on one of the displayed files.
-*/
-
-invoke("get_current_directory").then((dir) => currentDir.value = dir as string)
-
-invoke("get_current_files", { currentDir: currentDir.value })
-.then((files_vec) => files.value = files_vec as string[])
-.catch((error) => {
-  console.log(error)
-})
 
 const fileName = ref("")
 const fileContents = ref("")
@@ -33,34 +12,61 @@ const successfulWrite = ref(false)
 
 function createNewFile() {
   /* TO-DO (1)
-    Using Tauri commands, make a function that creates a new file in the current directory.
-    (HINT: use std::fs)
+      Using Tauri commands, make a function that creates a new file in the current directory.
+      (HINT: use std::fs - https://doc.rust-lang.org/std/fs/)
   */
 }
 
 function writeToFile() {
-  /* TO-DO (2)
-        Allow the user to write to the file once its been created.
+  /* TO-DO (3)
+      Create a function that writes contents to the file that has been created.
   */
 }
+
+function viewFile(file: string) {
+  
+}
+
+/* TO-DO (6) - bonus
+  Allow the user to navigate to different directories.
+  You will need to also show directories within the current directory and check if the
+  user clicks on a directory. If they do, show the contents of that directory to the screen.
+*/
 </script>
 
 <template>
   <div class="container">
     <h1>File Explorer</h1>
 
-    <p>Current directory is: {{ currentDir }}</p>
+    <div class="files">
 
-    <p v-for="file in files">
-      {{ file }}
-    </p>
+      <!-- TO-DO (4)
+        Show the contents of the current directory on the screen.
+        Once a new file is created by the user, it should also appear on the screen. 
+      -->
+      
+      <button v-for="file in files" @click="viewFile(file)">
+        {{ file }}
+      </button>
 
-    <input v-model="fileName" placeholder="File Name">
+      <!-- TO-DO (5)
+        Allow the user to view and edit any text file by clicking on one of the displayed files. 
+      -->
 
-    <button @click="createNewFile()">Create New File</button>
+      <br>
+
+      <input v-model="fileName" placeholder="File Name">
+
+      <br>
+
+      <button @click="createNewFile()">Create New File</button>
+    </div>
 
     <div v-if="showFile">
-      <p>{{ currentDir }}\{{ fileName }}</p>
+      <!-- TO-DO (2)
+        Show the absolute path to the newly created file.
+        (HINT: use std::env)
+      -->
       <textarea v-model="fileContents"></textarea>
       <button @click="writeToFile()">Save</button>
       <p v-if="successfulWrite">Saved!</p>
@@ -70,7 +76,18 @@ function writeToFile() {
 </template>
 
 <style scoped>
+
   ul {
     list-style-type: none;
   }
+  button {
+    width: 200px;
+  }
+
+  .files {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 </style>
+
